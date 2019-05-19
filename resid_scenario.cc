780c1872ec9ce4a2adef2d745a4bb3b804503022
kuba9605 <kuba9605@gmail.com>
2019-05-19 08:53:04 +0200

    initial commit

---
+++ resid_scenario.cc
@@ -0,0 +1,19 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("Residential_Scenario");
+
+int
+main (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.Parse (argc, argv);
+
+
+  return 0;
+}
