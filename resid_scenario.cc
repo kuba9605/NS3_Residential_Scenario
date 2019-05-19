e0129532ec2735daa47c0b37fb1e5faf56956905
kuba9605 <kuba9605@gmail.com>
2019-05-19 09:52:15 +0200

    Add nodes and devices

--- resid_scenario.cc
+++ resid_scenario.cc
@@ -6,6 +6,8 @@
 #include "ns3/applications-module.h"
 #include <ns3/buildings-module.h>
 
+#include <string>
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("Residential_Scenario");
@@ -16,11 +18,13 @@ main (int argc, char *argv[])
   uint32_t nFloors = 3;
   uint32_t xnFlats = 4;
   uint32_t ynFlats = 4;
+  uint32_t nSta = 1;
 
   CommandLine cmd;
   cmd.AddValue("nFloors", "Number of floors", nFloors);
   cmd.AddValue("xnFlats", "Number of flats per floor in x axis", xnFlats);
   cmd.AddValue("ynFlats", "Number of flats per floor in y axis", ynFlats);
+  cmd.AddValue("nSta", "Number of stations per floor", nSta);
   cmd.Parse (argc, argv);
 
   double x_min = 0.0;
@@ -36,6 +40,38 @@ main (int argc, char *argv[])
   b->SetNFloors (nFloors);
   b->SetNRoomsX (xnFlats);
   b->SetNRoomsY (ynFlats);
+
+  uint32_t nFlats = xnFlats * ynFlats * nFloors;
+  NodeContainer wifiStaNodes;
+  wifiStaNodes.Create(nFlats * nSta);
+  NodeContainer wifiApNodes;
+  wifiApNodes.Create(nFlats);
+
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default();
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default();
+  phy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
+  phy.SetChannel(channel.Create());
+
+  WifiHelper wifi;
+  wifi.SetStandard(WIFI_PHY_STANDARD_80211n_5GHZ);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("HtMcs7"), "ControlMode", StringValue("HtMcs0"));
+  WifiMacHelper mac;
+
+  NetDeviceContainer apDevices[nFlats];
+  NetDeviceContainer staDevices[nFlats][nSta];
+  Ssid ssid;
+  for(uint32_t i=0;i<nFlats;i++){
+    ssid = Ssid("network_" + std::to_string(i));
+    phy.Set("ChannelNumber", UintegerValue(36));
+
+    mac.SetType("ns3::ApWifiMac", "Ssid", SsidValue(ssid), "BeaconGeneration", BooleanValue(true));
+    apDevices[i] = wifi.Install(phy, mac, wifiApNodes.Get(i));
+
+    for(uint32_t j=0;j<nSta;j++){
+      mac.SetType("ns3::StaWifiMac", "Ssid", SsidValue(ssid));
+      staDevices[i][j] = wifi.Install(phy, mac, wifiStaNodes.Get(0));
+    }
+  }
 
   return 0;
 }
